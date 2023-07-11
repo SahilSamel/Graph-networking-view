6afb711a2995317887719d28d7c15f368ffdc728
Sahil Samel <sahilsamel134@gmail.com>
2023-07-11 11:27:49 +0530

    Added comments

--- back-end/connections/postgresConnection.js
+++ back-end/connections/postgresConnection.js
@@ -1,11 +1,12 @@
-import 'dotenv/config';
+import "dotenv/config";
 
+// Configuration for client connection to Postgres database
 const dbConfig = {
-    user: process.env.PostgresUSER,
-    password: process.env.PostgresPASS,
-    host: process.env.PostgresHOST,
-    database: process.env.PostgresDB,
-    port: process.env.PostgresPORT,
-  };
+  user: process.env.PostgresUSER,
+  password: process.env.PostgresPASS,
+  host: process.env.PostgresHOST,
+  database: process.env.PostgresDB,
+  port: process.env.PostgresPORT,
+};
 
 export default dbConfig;
