a105486f7bd1e92f6484cae9579dddef72df85f0
Sahil Samel <sahilsamel134@gmail.com>
2023-07-13 11:48:28 +0530

    Updated Auth flow

--- back-end/controller/authController.js
+++ back-end/controller/authController.js
@@ -10,68 +10,8 @@ import dbConfig from "../connections/postgresConnection.js";
 import driver from "../connections/neo4j.js";
 import jwt from "jsonwebtoken";
 
-// Database Entry for User
-const registerUser = async (req, res) => {
-  const {
-    uid,
-    email,
-    profImgURL,
-    userName,
-    name,
-    bio,
-    hobbies,
-    occupation,
-    education,
-  } = req.body;
-  const client = new Client(dbConfig);
 
-  try {
-    await client.connect();
-
-    const session = driver.session();
-    await session.run(
-      "CREATE (:User {userId: $uid, userName: $userName, profImgURL: $profImgURL}) ",
-      { uid, userName, profImgURL }
-    );
-    console.log("User saved successfully.");
-
-    const query = "SELECT create_user($1, $2, $3, $4, $5, $6, $7, $8, $9)";
-    const values = [
-      `${uid}`,
-      `${profImgURL}`,
-      `${userName}`,
-      `${name}`,
-      `${email}`,
-      `${bio}`,
-      `${hobbies}`,
-      `${occupation}`,
-      `${education}`,
-    ];
-    const result = await client.query(query, values);
-
-    console.log(result.rows[0].create_user);
-  } catch (error) {
-    console.error("An error occurred:", error.message);
-  } finally {
-    client.end();
-  }
-};
-
-// Check if user handle already in use
-const checkUserNameExists = async (userName) => {
-  const client = new Client(dbConfig);
-  try {
-    await client.connect();
-    const query = "SELECT check_user_exists($1)";
-    const values = [userName];
-    const result = await client.query(query, values);
-    console.log(result.rows[0].check_user_exists);
-  } catch (error) {
-    console.error("An error occurred:", error.message);
-  } finally {
-    client.end();
-  }
-};
+// <-- USER AUTH FUNCTIONS -->
 
 // Creating new User entry
 const createUser = (req, res) => {
@@ -88,23 +28,6 @@ const createUser = (req, res) => {
     .catch((error) => {
       res.status(409).json({ error: error.message });
     });
-};
-
-//check if uid exists in postgres db
-const googleUidCheck = async (req, res) => {
-  const { uid } = req.body;
-  const client = new Client(dbConfig);
-  try {
-    await client.connect();
-    const query = "SELECT check_uid_exists($1)";
-    const values = [uid];
-    const result = await client.query(query, values);
-    res.status(200).json({ result });
-  } catch (error) {
-    console.error("An error occurred:", error.message);
-  } finally {
-    client.end();
-  }
 };
 
 // Sign In with Email and Pass
@@ -131,4 +54,104 @@ const signIn = (req, res) => {
     });
 };
 
-export { createUser, registerUser, signIn, googleUidCheck };
+// Check if user logged In
+const checkLogin = (req, res) => {
+  if (req.cookies && req.cookies.token) {
+    res.sendStatus(200); 
+  } else {
+    res.sendStatus(401);
+  }
+}
+
+// <-- End of USER AUTH FUNCTIONS -->
+
+// <-- DATABASE FUNCTIONS -->
+
+// Database check for user information
+const uidDatabaseCheck = async (req, res) => {
+  const { uid } = req.body;
+  const client = new Client(dbConfig);
+  try {
+    await client.connect();
+    const query = "SELECT check_uid_exists($1)";
+    const values = [uid];
+    const result = await client.query(query, values);
+    if (result.rows[0].check_uid_exists) {
+      res.status(200).json({ status: "OK" });
+    } else {
+      res.status(404).json({ error: "User information not found" });
+    }
+  } catch (error) {
+    console.error("An error occurred:", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  } finally {
+    client.end();
+  }
+};
+
+// Database Entry for User
+const registerUser = async (req, res) => {
+  const {
+    uid,
+    email,
+    profImgURL,
+    userName,
+    name,
+    bio,
+    hobbies,
+    occupation,
+    education,
+  } = req.body;
+  const client = new Client(dbConfig);
+  
+  try {
+    await client.connect();
+    
+    const session = driver.session();
+    await session.run(
+      "CREATE (:User {userId: $uid, userName: $userName, profImgURL: $profImgURL}) ",
+      { uid, userName, profImgURL }
+      );
+      console.log("User saved successfully.");
+      
+      const query = "SELECT create_user($1, $2, $3, $4, $5, $6, $7, $8, $9)";
+      const values = [
+        `${uid}`,
+        `${profImgURL}`,
+        `${userName}`,
+        `${name}`,
+        `${email}`,
+        `${bio}`,
+        `${hobbies}`,
+      `${occupation}`,
+      `${education}`,
+    ];
+    const result = await client.query(query, values);
+    
+    console.log(result.rows[0].create_user);
+  } catch (error) {
+    console.error("An error occurred:", error.message);
+  } finally {
+    client.end();
+  }
+};
+
+// Check if user handle already in use
+const checkUserNameExists = async (userName) => {
+  const client = new Client(dbConfig);
+  try {
+    await client.connect();
+    const query = "SELECT check_user_exists($1)";
+    const values = [userName];
+    const result = await client.query(query, values);
+    console.log(result.rows[0].check_user_exists);
+  } catch (error) {
+    console.error("An error occurred:", error.message);
+  } finally {
+    client.end();
+  }
+};
+
+// <-- End of DATABASE FUNCTIONS -->
+
+export { createUser, registerUser, checkLogin, signIn, uidDatabaseCheck };
--- back-end/routes/authRoutes.js
+++ back-end/routes/authRoutes.js
@@ -1,24 +1,46 @@
-import { createUser, registerUser, signIn, googleUidCheck } from "../controller/authController.js"
-import express from "express"
+import {
+  createUser,
+  registerUser,
+  checkLogin,
+  signIn,
+  uidDatabaseCheck,
+} from "../controller/authController.js";
+import express from "express";
 import verifyToken from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
-router.post("/signup",(req,res)=>{
-    createUser(req,res);
-})
+// <-- USER AUTH FUNCTIONALITIES -->
 
-router.post("/createprofile",(req,res)=>{
-    registerUser(req,res);
-})
+// Sign up user using email/pass
+router.post("/signup", (req, res) => {
+  createUser(req, res);
+});
 
-router.post("/signin",(req,res)=>{
-    signIn(req,res);
-})
+// Sign in user using email/pass
+router.post("/signin", (req, res) => {
+  signIn(req, res);
+});
 
-router.post("/google",(req,res)=>{
-    googleUidCheck(req,res);
-})
+// Check if user already logged in
+router.get("/checkLogin", (req, res) => {
+  checkLogin(req, res);
+});
 
+// <-- End of USER AUTH FUNCTIONALITIES -->
+
+// <-- DATABASE FUNCTIONALITIES -->
+
+// Create database entry for user
+router.post("/createProfile", (req, res) => {
+  registerUser(req, res);
+});
+
+// Check if entry for user exists in database
+router.post("/dbCheck", (req, res) => {
+  uidDatabaseCheck(req, res);
+});
+
+// <-- End of DATABASE FUNCTIONALITIES -->
 
 export default router;
