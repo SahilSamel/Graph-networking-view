1afb713cd928bee412352872c890a15e0cab3785
Sahil Samel <sahilsamel134@gmail.com>
2023-07-17 15:36:53 +0530

    Graph displaying function updated for comms

--- front-end/components/Graph.tsx
+++ front-end/components/Graph.tsx
@@ -18,7 +18,7 @@ const Graph: React.FC = () => {
         console.log(err,"Error");
       } else{
         setGraphData(data);
-        setLoggedInUserId('2hrn2G4f6gTnGYeg5v5MCzgzCQ32');
+        setLoggedInUserId('wmuNSw3cknV1yTqjuUyxBhH9HE02');
       }
     });
   }, []);
@@ -33,7 +33,14 @@ const Graph: React.FC = () => {
           return node;
         }
       });
-      const data = { nodes: new DataSet<Node>(nodes), edges: new DataSet<Edge>(graphData.edges) };
+
+      console.log(nodes)
+  
+      const data = {
+        nodes: new DataSet<Node>(nodes),
+        edges: new DataSet<Edge>(graphData.edges),
+      };
+  
       const options = {
         physics: {
           enabled: true,
@@ -43,10 +50,11 @@ const Graph: React.FC = () => {
           },
         },
       };
+  
       const network = new Network(container!, data, options);
-
+  
       network.focus(loggedInUserId, {
-        scale: 1.5, 
+        scale: 1.5,
         animation: {
           duration: 1000,
           easingFunction: 'easeInOutQuart',
