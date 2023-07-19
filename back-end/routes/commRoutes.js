50708b83883726a7d7ab8de94d8493bc183e87e2
Sahil Samel <sahilsamel134@gmail.com>
2023-07-19 10:31:26 +0530

    Edit community route added

--- back-end/routes/commRoutes.js
+++ back-end/routes/commRoutes.js
@@ -1,4 +1,4 @@
-import { createCommunity } from "../controller/commController.js";
+import { createCommunity, editCommunity } from "../controller/commController.js";
 import express from "express";
 import verifyToken from "../middleware/verifyToken.js";
 
@@ -7,6 +7,11 @@ const router = express.Router();
 // Create Community Node
 router.post("/createCommunity", verifyToken, (req, res) => {
     createCommunity(req, res);
+});
+
+// Edit Community Node
+router.post("/editCommunity", verifyToken, (req, res) => {
+    editCommunity(req, res);
 });
 
 export default router;
